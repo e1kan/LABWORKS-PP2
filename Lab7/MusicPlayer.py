8add7acef20f1a96a3556c0e0e7ca03b526b5617
e1kan <156985217+e1kan@users.noreply.github.com>
2024-03-23 23:27:14 +0600

    Add files via upload

--- Lab7/MusicPlayer.py
+++ Lab7/MusicPlayer.py
@@ -1,28 +1,28 @@
 import pygame
+import random
+import os
 
-def play_next_song():
-    global _songs
-    _songs = _songs[1:] + [_songs[0]]
-    pygame.mixer.music.load(_songs[0])
-    pygame.mixer.music.play()
-
-
-def play_previous_song():
-    global _songs
-    _songs = [_songs[-1]] + [_songs[:-1]]
-    pygame.mixer.music.load(_songs[0])
+def shuffle():
+    global current_song, current_index, _songs
+    random_song = random.choice(_songs)
+    current_song = os.path.basename(random_song)
+    current_index = 5
+    pygame.mixer.music.load(random_song)
     pygame.mixer.music.play()
 
 pygame.init()
-surface = pygame.display.set_mode((900, 900))
+surface = pygame.display.set_mode((800, 798))
 pygame.display.set_caption("Music player")
 bg = pygame.image.load(r"C:\Users\Eva\Desktop\hello\University\LabWorks\Lab7\Music\bg.png")
-_songs = [r'C:\Users\Eva\Desktop\hello\University\LabWorks\Lab7\Music\haggstorm.mp3',
-          r'C:\Users\Eva\Desktop\hello\University\LabWorks\Lab7\Music\sweden.mp3',
-          r'C:\Users\Eva\Desktop\hello\University\LabWorks\Lab7\Music\aria_math.mp3',
-          r'C:\Users\Eva\Desktop\hello\University\LabWorks\Lab7\Music\subwoofer_lullaby.mp3',]
+_songs = ['C:\\Users\\Eva\\Desktop\\hello\\University\\LabWorks\\Lab7\Music\\haggstorm.mp3',
+          'C:\\Users\\Eva\\Desktop\\hello\\University\\LabWorks\\Lab7\Music\\sweden.mp3',
+          'C:\\Users\\Eva\\Desktop\\hello\\University\\LabWorks\\Lab7\Music\\aria_math.mp3',
+          'C:\\Users\\Eva\\Desktop\\hello\\University\\LabWorks\\Lab7\\Music\\subwoofer_lullaby.mp3',]
 done = False
 playing = False
+current_index = -1
+
+font = pygame.font.Font(r"C:\Users\Eva\Desktop\hello\University\LabWorks\Lab7\Music\Minecraftia.ttf", 20)
 
 while not done:
         for event in pygame.event.get():
@@ -32,14 +32,31 @@ while not done:
         surface.blit(bg, (0,0))
 
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_RIGHT]: play_next_song()
-        if pressed[pygame.K_LEFT]: play_previous_song()
-        if pressed[pygame.K_SPACE] and not playing:
-               playing = True
-               pygame.mixer.music.play()
-        if pressed[pygame.K_SPACE] and playing:
-               playing = False
-               pygame.mixer.music.stop()
+        if pressed[pygame.K_RIGHT]:
+            current_index = (current_index + 1) % len(_songs)
+            pygame.mixer.music.load(_songs[current_index])
+            pygame.mixer.music.play()
+            pygame.time.delay(150)
+        if pressed[pygame.K_LEFT]:
+            current_index = (current_index - 1) % len(_songs)
+            pygame.mixer.music.load(_songs[current_index])
+            pygame.mixer.music.play()
+            pygame.time.delay(150)
+        if pressed[pygame.K_s]:
+            shuffle()
+            pygame.time.delay(150)
+        if pressed[pygame.K_SPACE]:
+            if playing:
+                pygame.mixer.music.pause()
+                playing = False
+            else:
+                pygame.mixer.music.unpause()
+                playing = True
+            pygame.time.delay(150)
 
+        if current_index != 5:
+            current_song = os.path.basename(_songs[current_index])
+        text = font.render(current_song, True, (160, 160, 160))
+        surface.blit(text, (177, 663))
 
         pygame.display.flip()
